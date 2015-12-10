4d2d727c94f894f1f9c92f917cd44ce0b8a7e26d
Christophe Pelé <cpele@xebia.fr>
2015-12-10 13:05:12 +0100

    Add editable text with cleanup button

--- app/src/main/java/com/github/geekarist/fragmentssandbox/MainActivity.java
+++ app/src/main/java/com/github/geekarist/fragmentssandbox/MainActivity.java
@@ -3,8 +3,10 @@ package com.github.geekarist.fragmentssandbox;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View viewById = findViewById(R.id.button_next);
-        viewById.setOnClickListener(new View.OnClickListener() {
+        View nextButton = findViewById(R.id.button_next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
+
+        View cleanUpButton = findViewById(R.id.button_cleanup);
+        cleanUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cleanUpText();
+            }
+        });
+    }
+
+    private void cleanUpText() {
+        EditText nameText = (EditText) findViewById(R.id.text_name);
+        String text = nameText.getText().toString();
+        nameText.setText(text.toUpperCase());
     }
 }
